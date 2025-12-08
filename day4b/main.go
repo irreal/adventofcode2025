f20c4b48ff4a7d130d98e44518daa6c913929fbd
Miloš Spasojević <milos.spasojevic@nytimes.com>
2025-12-08 21:38:00 +0100

    day4 completed

---
+++ day4b/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func main() {
+	rows := strings.Split(input, "\n")
+	arr := make([][]int, len(rows))
+	outputArr := make([][]string, len(rows))
+
+	for idx, row := range rows {
+		arr[idx] = make([]int, len(row))
+		outputArr[idx] = make([]string, len(row))
+		for i, char := range row {
+			outputArr[idx][i] = string(char)
+			if char == '.' {
+				arr[idx][i] = 0
+			} else {
+				arr[idx][i] = 1
+			}
+		}
+	}
+	countTotal := 0
+	changed := true
+	for changed {
+		count := 0
+		changed = false
+		for y, row := range arr {
+			for x := range row {
+				if arr[y][x] == 1 && getNumberOfOnesSurrounding(arr, x, y) < 4 {
+					changed = true
+					outputArr[y][x] = "X"
+					arr[y][x] = 0
+					count++
+				}
+			}
+		}
+		fmt.Printf("Count: %d\n", count)
+		countTotal += count
+	}
+	fmt.Printf("Final count: %d", countTotal)
+}
+
+func getRelativeCoordinates(arr [][]int, x int, y int, deltaX int, deltaY int) int {
+	newY := y + deltaY
+	newX := x + deltaX
+
+	if newY < 0 || newY >= len(arr) || newX < 0 || (len(arr) > 0 && newX >= len(arr[0])) {
+		return 0
+	}
+	return arr[newY][newX]
+}
+
+func getSurroundingCoordinates(arr [][]int, x int, y int) []int {
+	return []int{
+		getRelativeCoordinates(arr, x, y, -1, -1),
+		getRelativeCoordinates(arr, x, y, 0, -1),
+		getRelativeCoordinates(arr, x, y, 1, -1),
+		getRelativeCoordinates(arr, x, y, -1, 0),
+		getRelativeCoordinates(arr, x, y, 1, 0),
+		getRelativeCoordinates(arr, x, y, -1, 1),
+		getRelativeCoordinates(arr, x, y, 0, 1),
+		getRelativeCoordinates(arr, x, y, 1, 1),
+	}
+}
+
+func getNumberOfOnesSurrounding(arr [][]int, x int, y int) int {
+	surrounding := getSurroundingCoordinates(arr, x, y)
+	count := 0
+	for _, cell := range surrounding {
+		if cell == 1 {
+			count++
+		}
+	}
+	return count
+}
